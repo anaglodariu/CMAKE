a496e772e370412a724d38e67eb5ab56a45f97bc
anaglodariu <anaglodariu@gmail.com>
2024-07-14 17:30:09 +0300

    set warnings as errors

--- build/CMakeFiles/Project.dir/flags.make
+++ build/CMakeFiles/Project.dir/flags.make
@@ -6,5 +6,5 @@ CXX_DEFINES =
 
 CXX_INCLUDES = 
 
-CXX_FLAGS = -std=c++20
+CXX_FLAGS = -std=c++20 -Wall -Wextra -Werror
 
--- program.cpp
+++ program.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 
 using namespace std;
-
+int function(void *);
 int main()
 {
+  int a = 9;
+  int *ptr = &a;
+  function((void *)a);
   cout << "Hello world kittens!" << endl;
+}
+
+
+int function(void *ptr) {
+  void *sth;
+  sth = malloc(sizeof(int)*4);
 }
